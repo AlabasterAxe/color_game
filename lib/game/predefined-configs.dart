058bfff385ff5b385d314890cc5d7913f3b21524
Jon <bedardjo@gmail.com>
2021-03-04 14:27:22 -0500

    simplify block replacement in default configs

--- lib/game/predefined-configs.dart
+++ lib/game/predefined-configs.dart
@@ -7,16 +7,16 @@ ColorGameConfig defaultConfig() => ColorGameConfig()..gridSize = Size(4, 4);
 
 ColorGameConfig easyToMakePlus() {
   List<GameBox> game = generateGameBoxes(colors: COLORS);
-  game.removeAt(21);
-  game.insert(21, GameBox(Offset(.5, .5), Colors.orange));
-  game.removeAt(16);
-  game.insert(16, GameBox(Offset(-.5, 1.5), Colors.orange));
-  game.removeAt(15);
-  game.insert(15, GameBox(Offset(-.5, .5), Colors.orange));
-  game.removeAt(14);
-  game.insert(14, GameBox(Offset(-.5, -.5), Colors.orange));
-  game.removeAt(7);
-  game.insert(7, GameBox(Offset(-1.5, -1.5), Colors.orange));
+  GameBox box = game.removeAt(21);
+  game.insert(21, GameBox(box.loc, Colors.orange));
+  box = game.removeAt(16);
+  game.insert(16, GameBox(box.loc, Colors.orange));
+  box = game.removeAt(15);
+  game.insert(15, GameBox(box.loc, Colors.orange));
+  box = game.removeAt(14);
+  game.insert(14, GameBox(box.loc, Colors.orange));
+  box = game.removeAt(7);
+  game.insert(7, GameBox(box.loc, Colors.orange));
   return ColorGameConfig()
     ..gridSize = Size(4, 4)
     ..predefinedGrid = game;
