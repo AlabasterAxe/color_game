57a946654091d51cc3710a670ff0ffa7733804ab
Jon <bedardjo@gmail.com>
2021-03-22 14:18:37 -0400

    size animation for stars.

--- lib/widgets/hud-stars-widget.dart
+++ lib/widgets/hud-stars-widget.dart
@@ -60,6 +60,7 @@ class _SpinningAnimatedStarState extends State<_SpinningAnimatedStar>
   Animation<double> angle = AlwaysStoppedAnimation(0);
   Animation<String> imageAsset =
       AlwaysStoppedAnimation("assets/images/star.png");
+  Animation<double> innerSizePct = AlwaysStoppedAnimation(1.0);
 
   @override
   void initState() {
@@ -73,16 +74,27 @@ class _SpinningAnimatedStarState extends State<_SpinningAnimatedStar>
 
   @override
   void didUpdateWidget(_SpinningAnimatedStar oldWidget) {
-    if (!oldWidget.earned && widget.earned) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.earned != widget.earned) {
       imageAsset = TweenSequence([
         TweenSequenceItem(
-            tween: ConstantTween("assets/images/star.png"), weight: .3),
+            tween: ConstantTween(widget.earned
+                ? "assets/images/star.png"
+                : "assets/images/gold_star.png"),
+            weight: .3),
         TweenSequenceItem(
-            tween: ConstantTween("assets/images/gold_star.png"), weight: .7)
+            tween: ConstantTween(widget.earned
+                ? "assets/images/gold_star.png"
+                : "assets/images/star.png"),
+            weight: .7)
       ]).animate(animation);
       angle = Tween(begin: 0.0, end: 2 * pi * 3)
           .animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut));
-      animation.forward();
+      innerSizePct = TweenSequence<double>([
+        TweenSequenceItem(tween: Tween(begin: 1.0, end: .5), weight: .5),
+        TweenSequenceItem(tween: Tween(begin: .5, end: 1.0), weight: .5)
+      ]).animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut));
+      animation.forward(from: 0);
     }
   }
 
@@ -90,10 +102,14 @@ class _SpinningAnimatedStarState extends State<_SpinningAnimatedStar>
   Widget build(BuildContext context) {
     return AnimatedBuilder(
         animation: animation,
-        builder: (context, child) => Transform(
+        builder: (context, child) => Container(
+            width: widget.size,
+            child: Center(
+                child: Transform(
               transform: Matrix4.rotationZ(angle.value),
               alignment: Alignment.center,
-              child: Image.asset(imageAsset.value, width: widget.size),
-            ));
+              child: Image.asset(imageAsset.value,
+                  width: widget.size * innerSizePct.value),
+            ))));
   }
 }
