aca2926b6c5fd7710ea28776869de0ba82608a18
Jon <bedardjo@gmail.com>
2021-04-09 15:07:06 -0400

    some slight improvements to end game dialog

--- lib/widgets/game-end-card.dart
+++ lib/widgets/game-end-card.dart
@@ -26,47 +26,65 @@ class GameEndCardState extends State<GameEndCard> {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double starSize = screenSize.width / 6;
+    String message = widget.earnedStars == 3
+        ? "Great Job!"
+        : widget.earnedStars == 2
+            ? "Nice Work!"
+            : "Good Job";
     return Card(
         shape: CARD_SHAPE,
         elevation: 4,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-                child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SpinningAnimatedStar(
-                        earned: widget.earnedStars > 0,
-                        size: 80,
-                        durationMS: 1300),
+        child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  message,
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.w800),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Container(
+                    height: starSize,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SpinningAnimatedStar(
+                              earned: widget.earnedStars > 0,
+                              size: starSize,
+                              durationMS: 1300),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          SpinningAnimatedStar(
+                              earned: widget.earnedStars > 1,
+                              size: starSize,
+                              durationMS: 1600),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          SpinningAnimatedStar(
+                              earned: widget.earnedStars > 2,
+                              size: starSize,
+                              durationMS: 1900),
+                        ])),
+                SizedBox(
+                  height: 32,
+                ),
+                Row(mainAxisSize: MainAxisSize.min, children: [
+                  ElevatedButton(
+                      child: Text("Retry"), onPressed: widget.onRetry),
+                  SizedBox(
+                    width: 16,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SpinningAnimatedStar(
-                        earned: widget.earnedStars > 1,
-                        size: 80,
-                        durationMS: 1600),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SpinningAnimatedStar(
-                        earned: widget.earnedStars > 2,
-                        size: 80,
-                        durationMS: 1900),
-                  ),
-                ])),
-            Row(mainAxisSize: MainAxisSize.min, children: [
-              ElevatedButton(child: Text("Retry"), onPressed: widget.onRetry),
-              SizedBox(
-                width: 16,
-              ),
-              ElevatedButton(child: Text("Back"), onPressed: widget.onBack)
-            ]),
-          ],
-        ));
+                  ElevatedButton(child: Text("Back"), onPressed: widget.onBack)
+                ]),
+              ],
+            )));
   }
 }
