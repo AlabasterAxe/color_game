3713b7b4e82085276d7d47085361ee5bf8a3d0f2
Jon <bedardjo@gmail.com>
2021-04-12 14:05:29 -0400

    constraints on card so it doesn't get too large

--- lib/widgets/game-end-card.dart
+++ lib/widgets/game-end-card.dart
@@ -46,83 +46,85 @@ class GameEndCardState extends State<GameEndCard> {
       "Nice Work!",
       "Great Job!"
     ][widget.earnedStars];
-    return Padding(
-        padding: EdgeInsets.all(32),
-        child: Card(
-            shape: CARD_SHAPE,
-            elevation: 4,
-            color: Colors.grey.shade600,
-            child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      message,
-                      style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    Container(
-                        height: starSize,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              SpinningAnimatedStar(
-                                  earned: widget.earnedStars > 0,
-                                  size: starSize,
-                                  durationMS: 1300),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              SpinningAnimatedStar(
-                                  earned: widget.earnedStars > 1,
-                                  size: starSize,
-                                  durationMS: 1600),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              SpinningAnimatedStar(
-                                  earned: widget.earnedStars > 2,
-                                  size: starSize,
-                                  durationMS: 1900),
-                            ])),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    Row(mainAxisSize: MainAxisSize.min, children: [
-                      Expanded(
-                          child: ColorCollapseButton(
-                              child: Text(
-                                "Retry",
-                                style: TextStyle(
-                                    fontSize: 32,
-                                    color: BOARD_BACKGROUND_COLOR,
-                                    fontWeight: FontWeight.w800),
-                                textAlign: TextAlign.center,
-                              ),
-                              onPressed: widget.onRetry)),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                          child: ColorCollapseButton(
-                              child: Text(
-                                "Next",
-                                style: TextStyle(
-                                    fontSize: 32,
-                                    color: BOARD_BACKGROUND_COLOR,
-                                    fontWeight: FontWeight.w800),
-                                textAlign: TextAlign.center,
-                              ),
-                              onPressed: widget.onBack))
-                    ]),
-                  ],
-                ))));
+    return ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 420),
+        child: Padding(
+            padding: EdgeInsets.all(32),
+            child: Card(
+                shape: CARD_SHAPE,
+                elevation: 12,
+                color: Colors.grey.shade600,
+                child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          message,
+                          style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        Container(
+                            height: starSize,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  SpinningAnimatedStar(
+                                      earned: widget.earnedStars > 0,
+                                      size: starSize,
+                                      durationMS: 1300),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  SpinningAnimatedStar(
+                                      earned: widget.earnedStars > 1,
+                                      size: starSize,
+                                      durationMS: 1600),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  SpinningAnimatedStar(
+                                      earned: widget.earnedStars > 2,
+                                      size: starSize,
+                                      durationMS: 1900),
+                                ])),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        Row(mainAxisSize: MainAxisSize.min, children: [
+                          Expanded(
+                              child: ColorCollapseButton(
+                                  child: Text(
+                                    "Retry",
+                                    style: TextStyle(
+                                        fontSize: 32,
+                                        color: BOARD_BACKGROUND_COLOR,
+                                        fontWeight: FontWeight.w800),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  onPressed: widget.onRetry)),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Expanded(
+                              child: ColorCollapseButton(
+                                  child: Text(
+                                    "Next",
+                                    style: TextStyle(
+                                        fontSize: 32,
+                                        color: BOARD_BACKGROUND_COLOR,
+                                        fontWeight: FontWeight.w800),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  onPressed: widget.onBack))
+                        ]),
+                      ],
+                    )))));
   }
 }
