5e745895f598a1cbb3c5e13caeefa5e089672ddf
Jon <bedardjo@gmail.com>
2021-04-12 13:50:04 -0400

    improve game end modal buttons a bit

--- lib/widgets/game-end-card.dart
+++ lib/widgets/game-end-card.dart
@@ -1,6 +1,7 @@
 import 'package:color_game/constants.dart';
 import 'package:color_game/main.dart';
 import 'package:color_game/services/audio-service.dart';
+import 'package:color_game/widgets/cc-button.dart';
 import 'package:color_game/widgets/spinning-animated-star.dart';
 import 'package:flutter/material.dart';
 
@@ -45,58 +46,83 @@ class GameEndCardState extends State<GameEndCard> {
       "Nice Work!",
       "Great Job!"
     ][widget.earnedStars];
-    return Card(
-        shape: CARD_SHAPE,
-        elevation: 4,
-        child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  message,
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.w800),
-                ),
-                SizedBox(
-                  height: 32,
-                ),
-                Container(
-                    height: starSize,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SpinningAnimatedStar(
-                              earned: widget.earnedStars > 0,
-                              size: starSize,
-                              durationMS: 1300),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          SpinningAnimatedStar(
-                              earned: widget.earnedStars > 1,
-                              size: starSize,
-                              durationMS: 1600),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          SpinningAnimatedStar(
-                              earned: widget.earnedStars > 2,
-                              size: starSize,
-                              durationMS: 1900),
-                        ])),
-                SizedBox(
-                  height: 32,
-                ),
-                Row(mainAxisSize: MainAxisSize.min, children: [
-                  ElevatedButton(
-                      child: Text("Retry"), onPressed: widget.onRetry),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  ElevatedButton(child: Text("Back"), onPressed: widget.onBack)
-                ]),
-              ],
-            )));
+    return Padding(
+        padding: EdgeInsets.all(32),
+        child: Card(
+            shape: CARD_SHAPE,
+            elevation: 4,
+            color: Colors.grey.shade600,
+            child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      message,
+                      style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Container(
+                        height: starSize,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SpinningAnimatedStar(
+                                  earned: widget.earnedStars > 0,
+                                  size: starSize,
+                                  durationMS: 1300),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              SpinningAnimatedStar(
+                                  earned: widget.earnedStars > 1,
+                                  size: starSize,
+                                  durationMS: 1600),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              SpinningAnimatedStar(
+                                  earned: widget.earnedStars > 2,
+                                  size: starSize,
+                                  durationMS: 1900),
+                            ])),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Row(mainAxisSize: MainAxisSize.min, children: [
+                      Expanded(
+                          child: ColorCollapseButton(
+                              child: Text(
+                                "Retry",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    color: BOARD_BACKGROUND_COLOR,
+                                    fontWeight: FontWeight.w800),
+                                textAlign: TextAlign.center,
+                              ),
+                              onPressed: widget.onRetry)),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                          child: ColorCollapseButton(
+                              child: Text(
+                                "Next",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    color: BOARD_BACKGROUND_COLOR,
+                                    fontWeight: FontWeight.w800),
+                                textAlign: TextAlign.center,
+                              ),
+                              onPressed: widget.onBack))
+                    ]),
+                  ],
+                ))));
   }
 }
